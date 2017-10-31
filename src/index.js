e644221c0546e298431f5aa0cdbafd2c83907b6b
juanjocerero <elquefaltaba@gmail.com>
2017-10-31 14:28:33 +0100

    parsing more

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import moment from 'moment'
 import path from 'path'
+import stringify from 'csv-stringify'
 import * as arr from 'd3-array'
 import * as fs from 'fs'
 
+const exportAsCsv = data => {
+  stringify(data, (error, output) => {
+    if (error) {
+      throw new Error(error)
+    }
+    try {
+      fs.writeFileSync(
+        path.join(
+          __dirname,
+          `/../output/ruins_granada.csv`
+        ),
+        output
+      )
+    } catch (error) {
+      throw new Error(error)
+    }
+  })
+}
+
 const csv = () => fs.readFileSync(
-  path.join(__dirname, './../41900-sevilla.geojson'),
+  path.join(__dirname, './../18900-granada.geojson'),
   { encoding: 'utf8' }
 )
 
@@ -18,6 +38,7 @@ const items = features.map(f => f.properties).map(p => ({
   buildingUnits: p.numberOfBuildingUnits,
   dwellings: p.numberOfDwellings,
   documentLink: p.documentLink,
+  informationSystem: p.informationSystem,
   area: `${p.value} ${p.value_uom}`
 }))
 
@@ -37,4 +58,8 @@ const ages = datesPlusAges.map(i => i.age).filter(i => i > 0)
 
 // console.log(ages.toString())
 
-console.log(arr.median(ages).toFixed(2))
+// console.log(arr.median(ages).toFixed(2))
+
+// console.log(items.filter(i => i.condition !== 'functional'))
+
+exportAsCsv(items.filter(i => i.condition === 'ruin'))
